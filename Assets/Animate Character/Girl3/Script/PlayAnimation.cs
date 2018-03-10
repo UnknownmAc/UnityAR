248cef1d2af100baa5c4f9052244077dae5fd630
Nishant Kumar <niskumar@adobe.com>
2018-03-10 21:23:41 +0530

    Hackathon live card demo

---
+++ Assets/Animate Character/Girl3/Script/PlayAnimation.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAnimation : MonoBehaviour {
+
+  
+    public Animator Ani;
+    public Sprite[] Spr;
+    public SpriteRenderer sprR;
+	public GameObject jerry;
+	private float oldScale = 0.4f;
+	private float i = 0.0f;
+   
+    public bool onlySpr = false;
+	// Use this for initialization
+	void Start () {
+		//jerry.transform.localScale += new Vector3 (oldScale + i, oldScale + i, oldScale + i);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//jerry.transform.localScale += new Vector3 (oldScale + i, oldScale + i, oldScale + i);
+	}
+
+    public void PlayAni(string str)
+    {
+       
+        Ani.enabled = true;
+        Ani.ForceStateNormalizedTime(0.0f);
+        Ani.Play(str);
+
+    }
+
+
+
+
+    public void ChangeSpr(int num)
+    {
+        if (onlySpr)
+        {
+            
+        }
+        else {
+           
+            Ani.ForceStateNormalizedTime(0.0f);
+            Ani.enabled = false;           
+        }
+
+        sprR.sprite = Spr[num];
+    }
+
+  
+
+}
---
+++ Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,141 @@
+/*==============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+///     A custom handler that implements the ITrackableEventHandler interface.
+/// </summary>
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    #region PRIVATE_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+
+    #endregion // PRIVATE_MEMBER_VARIABLES
+
+    #region UNTIY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+	public GameObject girl;
+	private float oldScale = 0.4f;
+	private float i = 0.01f;
+	private float targetTime = 3.0f;
+	bool objectFound = false;
+	int resizeCount = 2;
+
+	void transformGirl() {
+		girl.transform.localScale += new Vector3 (i, i, i);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		/*
+		if (objectFound) {
+			targetTime -= Time.deltaTime;
+
+			if (targetTime <= 0.0f) {
+				transformGirl ();
+				targetTime = 2.0f;
+
+				if (resizeCount % 3 == 0) {
+					i = i * -1;
+				}
+			}
+		}*/
+	}
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+			objectFound = true;
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NOT_FOUND)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+			objectFound = false;
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+			objectFound = false;
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PRIVATE_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+
+    #endregion // PRIVATE_METHODS
+}
